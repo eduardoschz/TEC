4a1297e9dbbd6556091fe924763e91d11520fc00
eduardoschz <jeduardo.schz@gmail.com>
2019-04-02 15:39:33 -0600

    Up to date

--- programa7/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $GLOBALS['db_connection'] = array(
-      'servername' => "localhost:3307",
-      'username' => "root",
-      'password' => "",
-      'dbname' => "continentes",
-      'charset' => 'utf8'
-    );
-?>
--- programa7/paises.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once './models/paises_model.php';
-$paises_array = get_paises();
-require_once './views/paises_view.php';
-?>
