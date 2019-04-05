be714671bb0a2349e7d038c04d9ab45de0fbc407
eduardoschz <jeduardo.schz@gmail.com>
2019-04-04 19:08:35 -0600

    Se anexan al proyecto los nuevos componentes.

---
+++ Ejemplo7/estado_add.php
@@ -0,0 +1,26 @@
+<?php
+if (!isset($_POST['estado'])) {
+  if (!isset($_GET['pais_id'])) {
+    echo "<h1>Falta Pais id</h1>";
+  }else{
+    require_once 'models/paises_model.php';
+    $pais_id = $_GET['pais_id'];
+    $pais = get_pais_by_id($pais_id);
+    require_once 'views/estado_form_view.php';
+  }
+}else{
+  $estado['pais_id'] = $_GET['pais_id'];
+  $estado['estado'] = $_POST['estado'];
+  $estado['poblacion'] = $_POST['poblacion'];
+  $estado['status'] = 1;
+  $estado['created_at'] = date('Y-m-d H:i:s');
+  $estado['updated_at'] = date('Y-m-d H:i:s');
+  require_once 'models/estados_model.php';
+
+  if (insert_estado($estado)) {
+    echo '<br /> <h1>Insertado</h1>';
+  }else{
+    echo '<br /> <h1>No insertado</h1>';
+  }
+}
+ ?>
---
+++ Ejemplo7/views/estado_form_view.php
@@ -0,0 +1,8 @@
+<h1>Agregar Estado a <?php  echo $pais['pais'] ?></h1>
+<form method="post">
+  <label for="estado">Nombre del estado: </label>
+  <input type="text" name="estado" placeholder="Escribe aqui el pais">
+  <label for="poblacion">Numero de habitantes: </label>
+  <input type="number" name="poblacion" placeholder="Escribe aqui el pais">
+  <button type="submit">Agregar</button>
+</form>
