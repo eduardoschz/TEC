1eac62cb3da67dfb65bdfd77156cb5bef273ffae
eduardoschz <jeduardo.schz@gmail.com>
2019-04-04 19:10:08 -0600

    Modulo corregido.

--- Ejemplo7/models/estados_model.php
+++ Ejemplo7/models/estados_model.php
@@ -3,10 +3,12 @@ require_once './config/functions.php';
 
 function get_estados_by_pais_id($pais_id){
   $consulta = "SELECT estado, poblacion FROM estados";
-  $consulta .= " WHERE paises_id = ".$pais_id.";";
-  //echo $consulta2. '<br />';
+  $consulta .= " WHERE pais_id = ".$pais_id.";";
   return get_elements($consulta);
 }
 
+function insert_estado($estado){
+    return insert_element('estados', $estado);
+}
 
  ?>
