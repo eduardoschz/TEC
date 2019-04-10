89903a18225bcdc8729f00f5556adb66089b1ee4
eduardoschz <jeduardo.schz@gmail.com>
2019-04-09 22:19:36 -0500

    Se guardan los atributos al objeto pais_ed para actualizar

--- Ejemplo7/pais_edit.php
+++ Ejemplo7/pais_edit.php
@@ -3,8 +3,11 @@ if (!isset($_POST['pais'])) {
   require_once 'views/pais_edit_form_view.php';
 }else{
   require_once 'models/paises_model.php';
-  print_r("->".$pais);
-  if (update_pais($pais, $id)) {
+  $pais_ed['pais'] = $_POST['pais'];
+  $pais_ed['fecha_indep'] = $_POST['indep'];
+  $pais_ed['updated_at'] = date('Y-m-d H:i:s');
+  $id = $_GET['pais_id'];
+  if (update_pais($pais_ed, $id)) {
     echo '<br /> <h1>Guardado</h1>';
   }else{
     echo '<br /> <h1>No guardado</h1>';
