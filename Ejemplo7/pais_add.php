f4594bb90a2cc19983a6fdd92d34883c8157c19b
eduardoschz <jeduardo.schz@gmail.com>
2019-04-10 16:29:08 -0500

    Ya inserta los idiomas seleccionados en los checkbox

--- Ejemplo7/pais_add.php
+++ Ejemplo7/pais_add.php
@@ -17,13 +17,15 @@ if (!isset($_POST['pais'])) {
 
   if ($pais_id) {
     require_once './models/paises_idiomas_model.php';
-    foreach ($_POST['idiomas'] as $idioma_id => $valor) {
-      $idioma_pais['idioma_id'] = $idioma_id;
-      $idioma_pais['pais_id'] = $pais_id;
-      $idioma_pais['status'] = 1;
-      $idioma_pais['created_at'] = date('Y-m-d H:i:s');
-      $idioma_pais['updated_at'] = date('Y-m-d H:i:s');
-      //print_r("idioma pais: ". $idioma_pais);
+    if ($_POST['idiomas']>0) {
+      foreach ($_POST['idiomas'] as $idioma_id => $valor) {
+        $idioma_pais['idioma_id'] = $idioma_id;
+        $idioma_pais['pais_id'] = $pais_id;
+        $idioma_pais['status'] = 1;
+        $idioma_pais['created_at'] = date('Y-m-d H:i:s');
+        $idioma_pais['updated_at'] = date('Y-m-d H:i:s');
+        //print_r("idioma pais: ". $idioma_pais);
+      }
       insert_pais_idioma($idioma_pais);
     }
     echo '<br /> <h1>Insertado</h1>';
