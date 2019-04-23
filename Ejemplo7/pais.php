0dfac5e006c357b28dca7ec0c3592ac4170d96ec
eduardoschz <jeduardo.schz@gmail.com>
2019-04-23 01:38:01 -0500

    Elimine linea para ver lo que viene en el POST y comenté una linea

--- Ejemplo7/pais.php
+++ Ejemplo7/pais.php
@@ -1,14 +1,26 @@
 <?php
+require_once './models/paises_idiomas_model.php';
+require_once './models/idiomas_model.php';
 require_once './models/paises_model.php';
 require_once './models/estados_model.php';
 if (isset($_GET['id'])) {
   $pais_id = $_GET['id'];
   $pais = get_pais_by_id($pais_id);
+
   $pais['estados'] = get_estados_by_pais_id($pais_id);
+  $pais['idiomas'] = get_idiomas_by_pais_id($pais_id);
   $pais['poblacion'] = 0;
+
+  //$idiomas_id_array = get_idiomas_id_by_pais_id($pais_id); //Se obtienen los id de los idiomas que habla un pais
+  //foreach ($idiomas_id_array[0] as $idioma_id => $id) {
+
+  //}
+
   foreach ($pais['estados'] as $estado) {
     $pais['poblacion'] += $estado['poblacion'];
   }
+
+
   require_once './views/pais_view.php';
 }else{
   echo "Falta id";
