0a1b4f9ff63e5170b3f3eb11b7c6dc9f70f447cd
eduardoschz <jeduardo.schz@gmail.com>
2019-04-23 01:39:55 -0500

    Elimine linea para ver el post del form.

--- Ejemplo7/pais_add.php
+++ Ejemplo7/pais_add.php
@@ -4,21 +4,17 @@ if (!isset($_POST['pais'])) {
   $idiomas_array = get_idiomas();
   require_once 'views/pais_form_view.php';
 }else{
-  print_r($_POST);
   $pais['pais'] = $_POST['pais'];
   $pais['fecha_indep'] = $_POST['indep'];
   $pais['status'] = 1;
   $pais['created_at'] = date('Y-m-d H:i:s');
   $pais['updated_at'] = date('Y-m-d H:i:s');
   require_once 'models/paises_model.php';
-
   $pais_id = insert_pais($pais);
-
-
   if ($pais_id) {
     require_once './models/paises_idiomas_model.php';
-    if ($_POST['idiomas']>0) {
-      foreach ($_POST['idiomas'] as $idioma_id => $valor) {
+    if ($_POST['idiomas']>0) { //Si no se selecciona ninguna casilla
+      foreach ($_POST['idiomas'] as $idioma_id => $value) {
         $idioma_pais['idioma_id'] = $idioma_id;
         $idioma_pais['pais_id'] = $pais_id;
         $idioma_pais['status'] = 1;
