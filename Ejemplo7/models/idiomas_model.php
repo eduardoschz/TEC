7bf0a7568f32e10499e2cc983ddddb3cca9bfd9f
eduardoschz <jeduardo.schz@gmail.com>
2019-04-23 01:32:36 -0500

    Funciones añadidas para poder editar los paises.

--- Ejemplo7/models/idiomas_model.php
+++ Ejemplo7/models/idiomas_model.php
@@ -1,8 +1,17 @@
 <?php
 require_once './database/functions.php';
+
 function get_idiomas(){
   $consulta = 'SELECT id, idioma FROM idiomas';
   return get_elements($consulta);
 }
-
+/**
+ * [get_idiomas_by_id description]
+ * @param  [num] $id [description]
+ * @return [array]     [description]
+ */
+function get_idiomas_by_id($id){
+  $consulta = 'SELECT idioma FROM idiomas WHERE id= '. $id .' AND status = 1';
+  return get_elements($consulta);
+}
  ?>
