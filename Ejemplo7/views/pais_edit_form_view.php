25e0f5ce74235b3b01ab65113d7393bdb109e69f
eduardoschz <jeduardo.schz@gmail.com>
2019-04-23 01:40:34 -0500

    Lineas para pintar los paises y marcarlos en el form para editar.

--- Ejemplo7/views/pais_edit_form_view.php
+++ Ejemplo7/views/pais_edit_form_view.php
@@ -1,4 +1,5 @@
-<?php require_once './models/paises_model.php';
+<?php
+
 $id = $_GET['pais_id'];
 $pais_ed = get_pais_by_id($id);
 ?>
@@ -6,7 +7,20 @@ $pais_ed = get_pais_by_id($id);
 <form method="post">
   <label for="pais">Nombre de pais: </label>
   <input name="pais" placeholder="Escribe aqui el pais" value=<?php echo $pais_ed['pais']; ?>>
+  <br>
   <label for="indep">Fecha de independencia: </label>
   <input type="date" name="indep" value="<?php echo $pais_ed['fecha_indep'] ?>">
+  <br>
+
+  <?php foreach ($idiomas_array as $idioma) { ?>
+    <input type="checkbox" name="idiomas_ed[<?php echo $idioma['id'] ?>]"
+    <?php if(isset($idioma['marcado'])){ ?>
+      <?php if($idioma['marcado'] == TRUE){ ?>
+        <?php echo 'checked';   ?>
+      <?php } ?>
+    <?php  } ?>
+    <?php echo$idioma['idioma']."<br />" ?>
+  <?php  } ?>
+
   <button type="submit">Guardar</button>
 </form>
